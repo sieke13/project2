fb682cfd7620329e28cd85c549fedaf0ee617b8e
Dhamar Sanchez <dhamarsnchez@gmail.com>
2025-01-22 00:51:32 -0600

    updated code errors

--- client/src/components/SearchBar/SearchBar.jsx
+++ client/src/components/SearchBar/SearchBar.jsx
@@ -1,1 +1,33 @@
+import React, { useState } from 'react';
 
+function SearchBar() {
+  const [query, setQuery] = useState('');
+  const [results, setResults] = useState([]);
+
+  const handleSearch = async () => {
+    const response = await fetch(
+      `https://en.wikipedia.org/w/api.php?action=opensearch&search=${query}&format=json`
+    );
+    const data = await response.json();
+    setResults(data[1]);
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        placeholder="Search Wikipedia"
+      />
+      <button onClick={handleSearch}>Search</button>
+      <ul>
+        {results.map((result, index) => (
+          <li key={index}>{result}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default SearchBar;
